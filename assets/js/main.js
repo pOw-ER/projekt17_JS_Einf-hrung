44d56cd6b93e5bbc8eb645351bab57cf5d82d018
Enes Yilmaz <enesyilmaz60@hotmail.com>
2021-02-08 18:46:51 +0100

    fertig

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const section = document.getElementById('section');
+const colorInput = document.getElementById('palette')
+
+function changeBgc() {
+  section.style.backgroundColor = colorInput.value;
+}
